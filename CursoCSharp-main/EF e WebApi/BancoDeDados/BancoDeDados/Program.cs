520da25438987ef3caefd4e6d0ace1909bde8155
gutemberg nunes <gutembergunico10@gmail.com>
2022-10-08 01:00:21 -0300

    TreinamentoCesar

---
+++ CursoCSharp-main/EF e WebApi/BancoDeDados/BancoDeDados/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BancoDeDados {
+    internal class Program {
+        static void Main(string[] args) {
+            string connectionstring = "DESKTOP-CFSSNFE\\SQLEXPRESS;Database=CursoCSharp;User Id=User;Password=123;Encrypt=False; TrustServerCertificate=False;";
+            Funcionario funcionario = new Funcionario();
+            funcionario.ObterLista();
+            //funcionario.ExecutarStoreProcedure(connectionstring, 1);
+            //funcionario.ExecutarStoreProcedure(connectionstring, 2);
+            //funcionario.ExecutarStoreProcedure(connectionstring, 22);
+            //funcionario.InserirDados(connectionstring);
+        }
+    }
+}
